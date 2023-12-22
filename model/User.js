1234cac9be671e719d7014a43e7efd552b87b9ca
ITL2k3 <91226590+ITL2k3@users.noreply.github.com>
2023-12-22 10:59:41 +0700

    VD

--- model/User.js
+++ model/User.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const argon2 = require('argon2')
-const Model = require('./Model.js')
+const Model = require('./Model.js');
+const { use } = require('../controller/auth.js');
 
 
 class User extends Model{
@@ -19,6 +20,40 @@ class User extends Model{
     this.connection.query({ sql: getHashedPassword, values: [username]}, callback);
 
   }
+  createUser_info = async (fullname, email, birthday, job,organization,address,avatar, callback) => {
+    let stringQuery = 'insert into account (fullname, email, birthday, job, organization, address, avatar) values ?'
+    this.connection.query({sql: stringQuery,values: [fullname, email, birthday, job, organization, address, avatar]}, callback)
+  }
+
+  updateUser_fullname = async(fullname,userId, callback) => {
+    let stringQuery = `UPDATE account SET fullname = ? WHERE id = ${userId}`
+    this.connection.query({sql: stringQuery, values: [fullname]}, callback)
+  }
+  updateUser_email = async(email,userId, callback) => {
+    let stringQuery = `UPDATE account SET email = ? WHERE id = ${userId}`
+    this.connection.query({sql: stringQuery, values: [email]}, callback)
+  }
+  updateUser_birthday = async(birthday, userId, callback) => {
+    let stringQuery = `UPDATE account SET birthday = ? WHERE id = ${userId}`
+    this.connection.query({sql: stringQuery, values: [birthday]}, callback)
+  }
+  updateUser_job = async(job,userId, callback) => {
+    let stringQuery = `UPDATE account SET job = ? WHERE id = ${userId}`
+    this.connection.query({sql: stringQuery, values: [job]}, callback)
+  }
+  updateUser_organization = async(organization,userId, callback) => {
+    let stringQuery = `UPDATE account SET organization = ? WHERE id = ${userId}`
+    this.connection.query({sql: stringQuery, values: [organization]}, callback)
+  }
+  updateUser_address = async(address,userId, callback) => {
+    let stringQuery = `UPDATE account SET address = ? WHERE id = ${userId}`
+    this.connection.query({sql: stringQuery, values: [address]}, callback)
+  }
+  updateUser_avatar = async(avatar,userId, callback) => {
+    let stringQuery = `UPDATE account SET avatar = ? WHERE id = ${userId}`
+    this.connection.query({sql: stringQuery, values: [avatar]}, callback)
+  }
+  
 }
 
 const user = new User();
